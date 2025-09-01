1e17a217ae9cb909ddff9e65666fc043ef02fa1a
Diksha <diksha@macbookpro>
2025-09-01 23:05:46 +0300

    Header commit

--- addition.cpp
+++ addition.cpp
@@ -1,16 +1,20 @@
+// COMSC-210 | Lab 3 | Diksha Tara Natesan
+// IDE used: Terminal
+using namespace std;
 #include <iostream>
+
 int main() {
-  int a;
-  int b;
-  int check;
-  do {
-  std::cout << "Enter a positive integer: ";
-  std::cin >> a;
-  std::cout << "Enter another positive integer: ";
-  std::cin >> b;
-  std::cout << a << " + " << b << " = " << (a+b) << std::endl;
-  std::cout << "Enter 0 to add a new pair of integers together OR enter any number to quit." << std::endl;
-  std::cin >> check;
+    int a;
+    int b;
+    int check;
+    do {
+        cout << "Enter a positive integer: ";
+        cin >> a;
+        cout << "Enter another positive integer: ";
+        cin >> b;
+        cout << a << " + " << b << " = " << (a+b) << std::endl;
+        cout << "Enter 0 to add a new pair of integers together OR enter any number to quit." << std::endl;
+        cin >> check;
 }
 while (check == 0);
 
